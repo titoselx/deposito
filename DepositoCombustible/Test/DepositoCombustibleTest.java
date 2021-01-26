adaf2cbc6fce6fa906a4b29eab10af65b29787e0
titoselx <74183026+titoselx@users.noreply.github.com>
2021-01-26 19:19:02 +0100

    Prueba deposito nivel.

---
+++ DepositoCombustible/Test/DepositoCombustibleTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DepositoCombustibleTest {
+
+	@Test
+	public void testGetDepositoNivel() {
+		DepositoCombustible tank = new DepositoCombustible(40.0,0.0);
+		double test = tank.getDepositoNivel();
+		
+		assertSame(tank.getDepositoNivel(),test);
+		
+	}
+
+}
